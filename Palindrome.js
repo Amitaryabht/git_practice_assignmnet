20f76753ddd2f34c06d67352d173b3ea40ca2959
Amit Kumar Thakur <amitt2117gmail.com>
2022-11-02 21:37:44 +0530

    prime and palindrome code added

---
+++ Palindrome.js
@@ -0,0 +1,7 @@
+let str = "naman";
+let bag = "";
+for(i=str.length-1;i++){
+   bag+=str[i];
+}if (str==bag){
+	console.log("Palindrome");
+else console.log("Not palindrome");
---
+++ Prime.js
@@ -0,0 +1,9 @@
+let N = 13;
+let count = 0;
+ for(i=0;i<=N;i++){
+ 	if(N%1==0){
+ 		count++;
+ 	}
+ }if(count==2){
+ 	console.log("Prime");
+ }else console.log("Not Prime");
